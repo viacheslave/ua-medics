f310e3a6233842d58b3ee0ce48b52787440670c1
Viacheslav Zholnovach <vyajol@gmail.com>
2021-08-18 16:07:42 +0300

    formatting

--- tests/UA.Medics.Tests.Integration/FileDownloderTests.cs
+++ tests/UA.Medics.Tests.Integration/FileDownloderTests.cs
@@ -9,31 +9,31 @@ using Xunit;
 namespace UA.Medics.Tests.Integration
 {
 	[Trait("Category", "Integration")]
-  public class FileDownloderTests
+	public class FileDownloderTests
 	{
-    [Fact]
-    public async Task CsvRecords_Collection_Test()
+		[Fact]
+		public async Task CsvRecords_Collection_Test()
 		{
-      var httpClient = new HttpClient();
+			var httpClient = new HttpClient();
 
-      var downloader = new FileDownloader(httpClient);
-      var url = LinksProvider.LocationMap[DataSetType.LegalEntity];
+			var downloader = new FileDownloader(httpClient);
+			var url = LinksProvider.LocationMap[DataSetType.LegalEntity];
 
-      var pageParser = new PageParser(httpClient);
-      var links = await pageParser.GetLinks(url);
+			var pageParser = new PageParser(httpClient);
+			var links = await pageParser.GetLinks(url);
 
-      var entry = links.First();
+			var entry = links.First();
 
-      var records = downloader.GetCsvRecords<LegalEntityCsvRecord, LegalEntityCsvRecordMap>(entry.Link.AbsoluteUri);
+			var records = downloader.GetCsvRecords<LegalEntityCsvRecord, LegalEntityCsvRecordMap>(entry.Link.AbsoluteUri);
 
-      var enumerator = records.GetAsyncEnumerator();
+			var enumerator = records.GetAsyncEnumerator();
 
-      var result = await enumerator.MoveNextAsync();
-      var record = enumerator.Current;
+			var result = await enumerator.MoveNextAsync();
+			var record = enumerator.Current;
 
-      Assert.True(result);
-      Assert.NotNull(record);
-      Assert.NotEqual(Guid.Empty, record.Id);
-    }
+			Assert.True(result);
+			Assert.NotNull(record);
+			Assert.NotEqual(Guid.Empty, record.Id);
+		}
 	}
 }
